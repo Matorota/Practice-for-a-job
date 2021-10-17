159235e07cbee1377ed288fea2c0ff595be7843a
Matorota <78918727+Matorota@users.noreply.github.com>
2021-10-17 16:58:30 +0300

    Lerning Vue
    BRAIN == DEAD

---
+++ issues/Vue/App.vue
@@ -0,0 +1,35 @@
+ <script>
+        new Vue({
+            el: '#to_do',
+
+            data: {
+                isEditing: false,
+                todo: '',
+                todos: [],
+                selectedTodo: null
+            },
+
+            methods: {
+                storeTodo() {
+                    this.todos.push(this.todo)
+                    this.todo = ''
+                },
+
+                removeTodo(index) {
+                    this.todos.splice(index, 1)
+                },
+
+                updateTodo() {
+                    this.todos.splice(this.selectedIndex, 1, this.todo)
+                    this.todo = ''
+                    this.isEditing = false
+                },
+
+                editTodo(index, todo) {
+                    this.isEditing = true
+                    this.todo = todo
+                    this.selectedIndex = index
+                }
+            }
+        })
+    </script>
