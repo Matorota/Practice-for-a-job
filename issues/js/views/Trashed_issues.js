53256b234e6057aa77946c1d01dcd33f8d570f43
Matorota <78918727+Matorota@users.noreply.github.com>
2021-10-17 16:24:29 +0300

    Todo_list
    Finished html and css and did few scripts with JavaScript still trying to understand Vue Framework.

--- issues/js/views/Trashed_issues.js
+++ issues/js/views/Trashed_issues.js
@@ -9,7 +9,7 @@ export default class extends AbstractView {
     async getHtml() {
         return `
             <h1>Trashed issues</h1>
-            <p>Manage your privacy and configuration.</p>
+            <p>Your Trashed Issues.</p>
         `;
     }
 }
